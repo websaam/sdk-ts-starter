056b6ed2ac458b31b3932652673c12601375be31
Anson <anson@Ansons-MacBook-Pro-2.local>
2022-07-16 15:37:48 +0100

    fix build steps

--- bin/cli.js
+++ bin/cli.js
@@ -15,7 +15,7 @@ const runCommand = (command) => {
 const repoName = process.argv[2];
 const src = 'https://github.com/websaam/sdk-ts-starter.git';
 const gitCheckoutCommand = `git clone --depth 1 ${src} ${repoName}`;
-const installDepsCommand = `cd ${repoName} && yarn`;
+const installDepsCommand = `cd ${repoName} && yarn && yarn devHtml`;
 
 console.log(`Cloning the repository with name ${repoName}`);
 
